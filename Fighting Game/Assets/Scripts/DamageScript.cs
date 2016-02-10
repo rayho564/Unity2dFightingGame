cb3d795a3f7e1f4e4c2355190eaef0d10c4546a1
Raymond Ho <rho002@ucr.edu>
2016-02-10 12:59:59 -0800

    Forgot to add the scripts

---
+++ Fighting Game/Assets/Scripts/DamageScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.root != transform.root && col.tag != "Ground" && !col.isTrigger)
+        {
+            //Can add function of all player attacks so player doesn't take damage when attacking
+            if (!col.transform.GetComponent<PlayerControl>().damage)
+            {
+                col.transform.GetComponent<PlayerControl>().damage = true;
+
+                col.transform.root.GetComponentInChildren<Animator>().SetTrigger("Damage");
+
+                //add how much damage over here
+            }
+        }
+    }
+}
---
+++ Fighting Game/Assets/Scripts/EventHolder.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventHolder : MonoBehaviour {
+
+    PlayerControl pl;
+
+	void Start () {
+        pl = transform.root.GetComponent<PlayerControl>();
+	}
+	
+	public void ThrowProjectile()
+    {
+        pl.specialAttack = true;
+    }
+}
