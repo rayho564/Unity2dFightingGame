9adf81a9d0347565e0acb8c695f0b60d1b327f39
Raymond Ho <rho002@ucr.edu>
2016-02-12 16:20:26 -0800

    Working on the Ui created the basis for the start menu. Got confused by Event/Input Manager, but got the start button going with Unity UI.

---
+++ Fighting Game/Assets/Scripts/StartButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+
+    public void StartClicked()
+    {
+            Application.LoadLevel("Main");
+    }
+}
